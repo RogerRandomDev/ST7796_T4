9ae7fc58364e8873ac6f4b9a3cf5b7f5f2d8b1c6
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2025-01-23 22:26:57 -0700

    Add files via upload

---
+++ ST7796_T4.h
@@ -0,0 +1,33 @@
+/******************************************************************************
+*  ST7796_T4 library for driving an ST7796 screen via SPI with a Teensy 4/4.1
+*  Implements vsync and differential updates from a memory framebuffer.
+*
+*  Copyright (c) 2020 Arvind Singh.  All right reserved.
+*
+* This library is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public
+*  License as published by the Free Software Foundation; either
+*  version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public
+*  License along with this library; if not, write to the Free Software
+*  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*******************************************************************************/
+
+
+#ifndef _ST7796_T4_H_
+#define _ST7796_T4_H_
+
+// forward to the real header. 
+#include "./ST7796Driver.h"
+// #include "./ST7796Driver.cpp"
+
+#endif
+
+/** end of file */
+
